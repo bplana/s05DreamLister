087dd72381c3039d9650a868898845ffe5402952
BP <oh_strawberry@yahoo.com>
2017-02-10 17:00:25 -0800

    connected views

---
+++ s05DreamLister/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  s05DreamLister
+//
+//  Created by bernadette on 2/10/17.
+//  Copyright © 2017 Bernadette P. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+
+}
--- s05DreamLister/MainVC.swift
+++ s05DreamLister/MainVC.swift
@@ -8,18 +8,44 @@
 
 import UIKit
 
-class MainVC: UIViewController {
-
+class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+// add protocols: UITableViewDelegate, UITableViewDataSource
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var segment: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // set delegate & dataSource
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // add required methods (func) for UITableViewDelegate, UITableViewDataSource
+    // numberOfRowsInSection, numberOfSections, cellForRowAt indexPath
+    
+    // for now: "return UITableViewCell()"
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
     }
 
-
+    // for now: "return 0"
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    }
+    
+    // for now: "return 0"
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+    
 }
 
